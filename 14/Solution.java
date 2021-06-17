76d217ae05e06ad66cd2334938bcbc32779ee707
Alexander Goussas <agoussas@espol.edu.ec>
2021-06-17 09:44:14 -0500

    completed crystal solution

--- 14/Solution.java
+++ 14/Solution.java
@@ -3,18 +3,14 @@ import java.util.stream.IntStream;
 import java.util.stream.Collectors;
 
 class Solution {
-    public String commonPrefix(String s1, String s2) {
-        if (s2.startsWith(s1))
-            return s1;
-        else
-            return IntStream.range(0, s1.length() < s2.length() ? s1.length() : s2.length())
-                    .takeWhile(i -> s1.charAt(i) == s2.charAt(i))
-                    .mapToObj(i -> String.valueOf(s1.charAt(i)))
-                    .collect(Collectors.joining())
-                    .toString();
-    }
-
     public String longestCommonPrefix(String[] strs) {
-        return Arrays.stream(strs).reduce(strs[0], this::commonPrefix);
+        return Arrays.stream(strs).reduce(strs[0], (acc, s) -> {
+            if (s2.startsWith(s1)) return s1;
+            return IntStream.range(0, s1.length() < s2.length() ? s1.length() : s2.length())
+                .takeWhile(i -> s1.charAt(i) == s2.charAt(i))
+                .mapToObj(i -> String.valueOf(s1.charAt(i)))
+                .collect(Collectors.joining())
+                .toString();
+        });
     }
 }
--- 14/solution.cr
+++ 14/solution.cr
@@ -1,6 +1,30 @@
+require "spec"
+
 module Solution
+  extend self
+
   def longest_common_prefix(strs : Array(String)): String
     strs.each.reduce do |acc, s|
+      acc.each_char.zip(s.each_char)
+        .take_while {|(c, k)| c == k}
+        .map {|(c, k)| c}
+        .join
+    end
+  end
+end
+
+describe Solution do
+  describe "#longest_common_prefix" do
+    it "[\"flower\",\"flow\",\"flight\"]" do
+      (Solution.longest_common_prefix ["flower","flow","flight"]).should eq "fl"
+    end
+
+    it "[\"dog\",\"racecar\",\"car\"]" do
+      (Solution.longest_common_prefix ["dog","racecar","car"]).should eq ""
+    end
+
+    it "[\"ab\", \"a\"]" do
+      (Solution.longest_common_prefix ["ab", "a"]).should eq "a"
     end
   end
 end
